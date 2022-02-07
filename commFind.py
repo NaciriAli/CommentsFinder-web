aff49851a7df32241e23bb65e7c7134a3c962578
NaciriAli <alinaciri2002@gmail.com>
2022-02-08 00:34:42 +0100

    Initial commit

---
+++ commFind.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+commentsFinder = Flask(__name__)
+
+@commentsFinder.route("/")
+def home():
+    return render_template("home.html",title="Home",custom_css="home")
+
+@commentsFinder.route("/about")
+def about():
+    return render_template("about.html",title="About",custom_css="about")
+
+@commentsFinder.route("/contact")
+def contact():
+    return render_template("contact.html",title="Contact",custom_css="contact")
+
+if __name__ == "__main__":
+    commentsFinder.run(debug=True,port=9000)
